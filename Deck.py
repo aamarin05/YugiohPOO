8bc4ee1818d29bdb43ce52e0c6d2b65d37da1bd7
JuanPaManjarres <jupamanj@espol.edu.ec>
2024-11-25 21:57:44 -0500

    Update Deck.py

--- Deck.py
+++ Deck.py
@@ -1,22 +1,33 @@
-from cartasCreadas import listaCartasCreadas
 from CartaMonstruo import *
 from CartaTrampa import *
 from CartaMagica import *
+from TipoAtributo import *
+from TipoMonstruo import *
 import random as rd
 
-
-
 class Deck:
-  def crearDeck(self):#crea lista de cartas
+  def crearDeck(self):
     l_mons=[]
     l_mag=[]
     l_tram=[]
-    for c in listaCartasCreadas:
-      if isinstance(c,CartaMonstruo):
+    archivo= open("cartasCreadas",'r')
+    for linea in archivo.strip().split(','):
+      tipodecarta,nombre,descripcion,posicion,orientacion,tipomonstruo,tipoatributo,ataque,defensa,= linea
+      posicion= posicion.split('.')[-1]
+      orientacion= orientacion.split('.')[-1]
+      tipomonstruo= tipomonstruo.split('.')[-1]
+      tipoatributo= tipoatributo.split('.')[-1]
+      ataque=int(ataque)
+      defensa=int(defensa)
+      if(tipodecarta=="CartaMonstruo"):
+        c= CartaMonstruo(nombre,descripcion,Posicion[posicion],Orientacion[orientacion],TipoMonstruo[tipomonstruo],TipoAtributo[tipoatributo],defensa,ataque)
         l_mons.append(c)
-      if isinstance(c,CartaMagica):
+      if(tipodecarta=="CartaMagica"):
+        c= CartaMagica(nombre, descripcion, Posicion[posicion], Orientacion[orientacion], ataque, defensa)
         l_mag.append(c)
-      if isinstance(c,CartaTrampa):
+      if(tipodecarta=="CartaTrampa"):
+        c= CartaTrampa(nombre, descripcion, Posicion[posicion], Orientacion[orientacion],TipoAtributo[tipoatributo])
         l_tram.append(c)
-      deck= rd.sample(l_mons,20)+rd.sample(l_mag,5)+rd.sample(l_tram,5)
-    return deck
+      archivo.close()
+      deck=rd.sample(l_mons,20)+rd.sample(l_mag,5)+rd.sample(l_tram,5)
+      return deck
