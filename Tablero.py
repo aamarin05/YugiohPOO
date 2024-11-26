0da37e656b001759e94dace43e6a88a65930c565
JuanPaManjarres <jupamanj@espol.edu.ec>
2024-11-25 20:39:41 -0500

    Update Tablero.py

--- Tablero.py
+++ Tablero.py
@@ -1,3 +1,13 @@
+from Carta import *
+from Jugador import *
 class Tablero:
-  def _init_(self):
-    self.__cartas= []
+  def init(self):
+    self.__cartasjugador = [[None, None, None], [None, None, None]] 
+  def _str_(self):
+    print(f"Tablero
+            \nMonstruo: [{self._cartasjugador[0][0]}] [{self.cartasjugador[0][1]}] [{self._cartasjugador[0][2]}]
+            \nMagicas: [{self._cartasjugador[1][0]}] [{self.cartasjugador[1][1]}] [{self._cartasjugador[1][2]}]
+            ")
+  def seleccionarCarta(self,indice):
+    print(self._str_())
+    return self.__cartasjugador[1,indice]#Retorna la Carta del indice indicado
