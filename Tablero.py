9e90ea63b2c43446e1495094f48c9eb4ef4c36ab
JuanPaManjarres <jupamanj@espol.edu.ec>
2024-11-25 20:44:14 -0500

    Update Tablero.py

--- Tablero.py
+++ Tablero.py
@@ -5,8 +5,8 @@ class Tablero:
     self.__cartasjugador = [[None, None, None], [None, None, None]] 
   def _str_(self):
     print(f"Tablero
-            \nMonstruo: [{self._cartasjugador[0][0]}] [{self.cartasjugador[0][1]}] [{self._cartasjugador[0][2]}]
-            \nMagicas: [{self._cartasjugador[1][0]}] [{self.cartasjugador[1][1]}] [{self._cartasjugador[1][2]}]
+            \nMonstruo: [{self.__cartasjugador[0][0]}] [{self.__cartasjugador[0][1]}] [{self.__cartasjugador[0][2]}]
+            \nMagicas: [{self.__cartasjugador[1][0]}] [{self.__cartasjugador[1][1]}] [{self.__cartasjugador[1][2]}]
             ")
   def seleccionarCarta(self,indice):
     print(self._str_())
