d89fb148456d278f506984626a300dd07e190654
JuanPaManjarres <jupamanj@espol.edu.ec>
2024-11-25 20:48:08 -0500

    Update Jugador.py

--- Jugador.py
+++ Jugador.py
@@ -11,20 +11,14 @@ class Jugador:
 
   def getNombre(self):
     return self.__nombre
-  def getDeck (self):
-    return self.__deck
   def getPuntos (self):
     return self.__puntos
   def setPuntos (self, puntos):
     self.__puntos = puntos
   def getMano (self):
     return self.__mano
-
   def tomarCarta(self):
     return self.__deck.pop()
-  def contar_cartas_tipo(self, tipo):
-    for carta in self.__mano:
-      return sum(isinstance(carta, tipo))
   def agregarCartaTablero(self,carta):
       if isinstance(carta,CartaMonstruo):
         if None in self.__tablero.__cartasjugador[0]:
