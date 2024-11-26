b0d33a374e1682243919e2fe7c85d31abd9646cc
JuanPaManjarres <jupamanj@espol.edu.ec>
2024-11-25 23:23:35 -0500

    Update CartaTrampa.py

--- CartaTrampa.py
+++ CartaTrampa.py
@@ -12,5 +12,3 @@ class CartaTrampa (Carta):
   def getAtributo (self):
       return self.__atributo
 
-  def __str__(self):
-    return f"{self.__nombre} , detiene el ataque de un monstruo con atributo {self.__atributo}"
