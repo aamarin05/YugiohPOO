6449c51d482fe5323028026b05e0cc1599b84f1a
JuanPaManjarres <jupamanj@espol.edu.ec>
2024-11-26 20:40:43 -0500

    Update Tablero.py

--- Tablero.py
+++ Tablero.py
@@ -9,7 +9,6 @@ class Tablero:
     return self.__cartasjugador[1]
   def _str_(self):
     print(f"Tablero\nMonstruo: [{self.__cartasjugador[0][0].__str__()}] [{self.__cartasjugador[0][1].__str__()}] [{self.__cartasjugador[0][2].__str__()}]\nEspeciales: [{self.__cartasjugador[1][0].__str__()}] [{self.__cartasjugador[1][1].__str__()}] [{self.__cartasjugador[1][2].__str__()}]")
-    
   def seleccionarCarta(self,indice):
     print(self._str_())
     return self.__cartasjugador[1,indice]#Retorna la Carta del indice indicado
