93aa029a4cad9b0297045a3d69662168fff6963d
JuanPaManjarres <jupamanj@espol.edu.ec>
2024-11-26 23:43:43 -0500

    Update Deck.py

--- Deck.py
+++ Deck.py
@@ -14,13 +14,13 @@ class Deck:
     for linea in archivo:
       linea = linea.strip().split(',')
       if(linea[0]=="CartaMonstruo"):
-        c= CartaMonstruo(nombre=linea[1],descripcion=linea[2],posicion=linea[3].strip(""),orientacion=linea[4].strip(""),tipomonstruo= linea[5].strip(""),atributo= linea[6].strip(""),defensa= int(linea[7]),ataque= int(linea[8]))
+        c= CartaMonstruo(nombre=linea[1],descripcion=linea[2],posicion=Posicion[linea[3]],orientacion=Orientacion[linea[4]],tipomonstruo= TipoMonstruo[linea[5]],atributo= TipoAtributo[linea[6]],defensa= int(linea[7]),ataque= int(linea[8]))
         l_mons.append(c)
       if(linea[0]=="CartaMagica"):
-        c= CartaMagica(nombre=linea[1], descripcion=linea[2], posicion=linea[3].strip(""), orientacion=linea[4].strip(""), ataque= int(linea[5]), defensa= int(linea[6]),tipomonstruo= linea[7].strip(""))
+        c= CartaMagica(nombre=linea[1], descripcion=linea[2], posicion=Posicion[linea[3]], orientacion=Orientacion[linea[4]], ataque= int(linea[5]), defensa= int(linea[6]),tipomonstruo= TipoMonstruo[linea[5]])
         l_mag.append(c)
       if(linea[0]=="CartaTrampa"):
-        c= CartaTrampa(nombre=linea[1], descripcion=linea[2], posicion=linea[3].strip(""),  orientacion=linea[4].strip(""),atributo= linea[5].strip(""))
+        c= CartaTrampa(nombre=linea[1], descripcion=linea[2], posicion=Posicion[linea[3]], orientacion=Orientacion[linea[4]],atributo= TipoAtributo[linea[6]])
         l_tram.append(c)
       archivo.close()
       deck=rd.sample(l_mons,20)+rd.sample(l_mag,5)+rd.sample(l_tram,5)
