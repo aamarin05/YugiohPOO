801cf67b7cc1edca4c76c97218bdd6c920744ee8
JuanPaManjarres <jupamanj@espol.edu.ec>
2024-11-26 23:56:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ from Jugador import *
 from Maquina import *
 from Juego import *
 
-#if __name__ == "__main__":
-#  print("Bienvenido al juego de Yugioh")
-#  nombre = input ("Por favor, ingrese su nombre: ")
-#  jugador = Jugador(nombre)
-#  maquina = Maquina()
-#  juego = Juego(jugador, maquina)
-#  juego.jugar()
-carta= CartaMonstruo(CartaMonstruo,"Rey Bestia","El líder de las bestias salvajes",Posicion.VERTICAL,Orientacion.ABAJO,TipoMonstruo.B,TipoAtributo.TIERRA,1600,1900)
-print(carta.__str__())
+if __name__ == "__main__":
+  print("Bienvenido al juego de Yugioh")
+  nombre = input ("Por favor, ingrese su nombre: ")
+  jugador = Jugador(nombre)
+  maquina = Maquina()
+  juego = Juego(jugador, maquina)
+  juego.jugar()
