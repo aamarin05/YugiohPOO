1a2baa5ca9f152e1202731c2df383201e359b895
JuanPaManjarres <jupamanj@espol.edu.ec>
2024-11-26 20:30:18 -0500

    Update Tablero.py

--- Tablero.py
+++ Tablero.py
@@ -1,13 +1,10 @@
 from Carta import *
 from Jugador import *
 class Tablero:
-  def init(self):
+  def _init_(self):
     self.__cartasjugador = [["No hay Carta", "No hay Carta", "No hay Carta"], ["No hay Carta", "No hay Carta", "No hay Carta"]] 
   def _str_(self):
-    print(f"Tablero
-            \nMonstruo: [{self.__cartasjugador[0][0].__str__()}] [{self.__cartasjugador[0][1].__str__()}] [{self.__cartasjugador[0][2].__str__()}]
-            \nEspeciales: [{self.__cartasjugador[1][0].__str__()}] [{self.__cartasjugador[1][1].__str__()}] [{self.__cartasjugador[1][2].__str__()}]
-            ")
+    print(f"Tablero\nMonstruo: [{self.__cartasjugador[0][0].__str__()}] [{self.__cartasjugador[0][1].__str__()}] [{self.__cartasjugador[0][2].__str__()}]\nEspeciales: [{self.__cartasjugador[1][0].__str__()}] [{self.__cartasjugador[1][1].__str__()}] [{self.__cartasjugador[1][2].__str__()}]")
     
   def seleccionarCarta(self,indice):
     print(self._str_())
